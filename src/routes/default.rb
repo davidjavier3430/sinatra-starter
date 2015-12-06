b90994c31bb83d1da9034811553863a35ee5c183
davidjavier3430 <davidjavier3430@gmail.com>
2015-12-06 10:55:12 -0400

    add members.count

--- src/routes/default.rb
+++ src/routes/default.rb
@@ -16,19 +16,30 @@ class MyApplication < Sinatra::Base
   post "/rides/new" do
     @title = "Create Route"
     @hide_create_route = true
-    @ride = Ride.create( params["ride"] )
+    @ride = Ride.new( params["ride"] )
+    @ride.members = JSON.generate({ members: [] })
+    @ride.save
     if @ride.errors.size > 0
       erb :new
     else
       redirect "/rides/#{@ride.id}"
     end
+  end
 
+  post "/rides/:id/members" do
+    @ride = Ride.find(params['id'])
+    @ride.members['members'].push({'email': params['ride']['members']})
+    @ride.save
+    if @ride.errors.empty?
+      redirect "/rides/#{@ride.id}"
+    end
   end
 
   get "/rides/:id" do
     @title = "Route Info"
     ride_id =  params[:id]
     @ride = Ride.find(ride_id)
+    @members_count = @ride.members['members'].count + 1
     @miles = '%.2f' % @ride.route['meters'].meters.to.miles
     erb :show
   end
