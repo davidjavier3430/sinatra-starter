7d910035142fe434e0679d1df8762fbfd7a7bb34
davidjavier3430 <davidjavier3430@gmail.com>
2015-12-09 04:37:04 -0400

    update app.rb

--- src/app.rb
+++ src/app.rb
@@ -11,6 +11,8 @@ require "sinatra/flash"
 require "sinatra/content_for"
 require "will_paginate"
 require 'will_paginate/active_record'
+require 'alchemist'
+Alchemist.setup
 
 #Require Helpers
 Dir[File.dirname(__FILE__) + '/helpers/*.rb'].each {|file| require file }
@@ -20,6 +22,7 @@ Dir[File.dirname(__FILE__) + '/models/*.rb'].each {|file| require file }
 
 #Require Routes
 Dir[File.dirname(__FILE__) + '/routes/*.rb'].each {|file| require file }
+
 
 class MyApplication < Sinatra::Base
 
